be471b3a67910b92263f75b8930c49cfc4b2de69
hughbirky <98499661+hughbirky@users.noreply.github.com>
2026-01-20 11:55:02 -0600

    Add AudioExample project

---
+++ AudioExample/AudioExample/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  AudioExample
+//
+//  Created by Hack Audio on 10/5/19.
+//  Copyright © 2019 Eric Tarr. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include "AudioFile/AudioFile.hpp"
+using namespace std;
+
+
+
+int main() {
+    
+    
+    
+    return 0;
+}
+
