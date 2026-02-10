4404fee36c65a15e7c03d9580861fc43ce0096d6
Eric Tarr <erictarrbelmont@gmail.com>
2026-02-10 12:31:30 -0600

    Lecture 5

--- AudioExample/AudioExample/main.cpp
+++ AudioExample/AudioExample/main.cpp
@@ -12,6 +12,23 @@
 #include "AudioFile/Sound.hpp"
 using namespace std;
 
+// User created structure
+struct AudioInfo {
+    string filename;
+    int Fs;
+    int bitDepth;
+    int numChannels;
+    int N;
+};
+
+void audioread(AudioInfo & info, vector<float> & signal){
+    
+    audioread(info.filename, signal, info.Fs, info.bitDepth, info.numChannels);
+    
+}
+
+
+
 void myGainFunction(vector<vector<float>> & sig, int N, float dBAmp){
     
     float A = powf(10.f,dBAmp/20.f); // A - linear amplitude
@@ -43,35 +60,57 @@ void myArrayFunction(float * array, int N){
     
 }
 
-// User created structure
-struct AudioInfo {
-    string filename;
-    int Fs;
-    int bitDepth;
-    int numChannels;
-    int N;
-};
+
 
 class GainEffect{
 public:
+    
+    GainEffect(){
+        dBAmp = 0.f;
+        A = 1.f;
+    }
+    
+    float dBAmp = 0.f; // decibel amplitude
+    
+    // Overloaded Constructor
+    GainEffect(float dBValue){
+        setdBAmp(dBValue);
+    }
+    
+
+    
+    void processSignal(vector<float> & signal, int N){
+        for (int n = 0; n < N ; ++n){
+            signal[n] = processSample(signal[n]);
+        }
+    }
+    
+    float processSample(float x){
+        return x * A;
+    }nike.
     
     void setdBAmp(float dBValue){
         dBAmp = dBValue;
         A = pow(10.f,dBAmp/20.f);
     }
     
-    float dBAmp; // decibel amplitude
-    float A; // linear amplitude
+    
+    float A = 1.f; // linear amplitude
 };
 
 int main() {
     
     AudioInfo info;
-    info.filename = "stereoDrums.wav";
+    info.filename = "AcGtr.wav";
     
-    vector<vector<float>> signal;
+    vector<float> signal;
     
-    audioread(info.filename, signal, info.Fs, info.bitDepth, info.numChannels);
+    audioread(info,signal);
+    
+    // OLD WAY without structure
+    //audioread(info.filename, signal, info.Fs, info.bitDepth, info.numChannels);
+    
+    
     
     //sound(signal,Fs,bitDepth,numChannels);
     
@@ -80,14 +119,23 @@ int main() {
         info.N = signal.size();
     }
     else{
-        info.N = signal[0].size();
+        //info.N = signal[0].size();
     }
     
-    GainEffect gain;
-    gain.setdBAmp(-18.f);
+    GainEffect gain2;
+    gain2.setdBAmp(6.f);
+    gain2.processSignal(signal, info.N);
     
     
-    myGainFunction(signal, info.N, -18.f);
+    
+    GainEffect gain {-18.f}; // constructor initialized to -18 dB
+    //gain.setdBAmp(-18.f);
+    gain.processSignal(signal,info.N);
+    
+    sound(signal, info.Fs, info.bitDepth, info.numChannels);
+    
+    
+    //myGainFunction(signal, info.N, -18.f);
     
     
     vector<float> monoSignal;
